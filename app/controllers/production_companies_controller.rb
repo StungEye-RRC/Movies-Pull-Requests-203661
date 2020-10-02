6f28ddaf03a99f2b6ad61e0db93f02c40856805b
Kyle Geske <stungeye@gmail.com>
2020-10-02 14:36:36 -0500

    Now ordering production companies by the number of movie they made.

--- app/controllers/production_companies_controller.rb
+++ app/controllers/production_companies_controller.rb
@@ -1,7 +1,14 @@
 class ProductionCompaniesController < ApplicationController
   def index
-    @production_companies = ProductionCompany.all
+    @production_companies = ProductionCompany.ordered_by_movies
   end
+
+  # SELECT COUNT(production_companies.id), production_companies.id, production_companies.name
+  # FROM "production_companies"
+  # LEFT OUTER JOIN "movies"
+  # 	ON "movies"."production_company_id" = "production_companies"."id"
+  # GROUP BY "production_companies"."id"
+  # ORDER BY movie_count DESC;
 
   def show
     @production_company = ProductionCompany.find(params[:id])
