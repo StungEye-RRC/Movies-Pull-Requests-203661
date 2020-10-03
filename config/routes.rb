502e7c9a2683307f338b76376f6d93e9f48a2258
Kyle Geske <stungeye@gmail.com>
2020-10-02 21:45:54 -0500

    CMS Pages with permalinks.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :pages
+  resources :pages, except: [:show]
+
+  get "/pages/:permalink" => "pages#permalink", as: :permalink
+
   resources :movies, only: %i[index show]
   resources :production_companies, only: %i[index show]
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,19 @@ require "csv"
 
 Movie.delete_all
 ProductionCompany.delete_all
+Page.delete_all
+
+Page.create(
+  title:     "About the Data",
+  content:   "The data powering this website was provided by a Kaggle dataset about the IMDB movie ratings.",
+  permalink: "about_the_data"
+)
+
+Page.create(
+  title:     "Contact Us",
+  content:   "If you want to chat about movies email me at wally.glutton@mailinator.com.",
+  permalink: "contact_us"
+)
 
 filename = Rails.root.join("db/top_movies.csv")
 
